2f17e9989e27fa5460231670a70426f264e7c610
rKeshaw <101080156+rKeshaw@users.noreply.github.com>
2025-11-26 19:38:35 +0530

    Add files via upload

---
+++ 900/bad_boy.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+
+  while (t--) {
+    long long n, m, i, j;
+    cin >> n >> m >> i >> j;
+    cout << 1 << " " << 1 << " " << n << " " << m << endl;
+  }
+}
